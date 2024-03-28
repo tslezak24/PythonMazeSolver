2362c698c02eafbd5f93b5e52504ed52d8d472ca
tslezak24 <tmslezak24@gmail.com>
2024-03-28 14:55:44 -0500

    Initial commit and draw line functionality

---
+++ window.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, BOTH, Canvas
+from line import Line
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title = 'This is the Title'
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.is_running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+        
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+    
+    def close(self):
+        self.is_running = False
+        
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+        
